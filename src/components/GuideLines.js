a73782ed6095f37d8d0781ea645875bbc3282231
rnfltpgus <jpp901220@gmail.com>
2022-07-13 02:17:40 +0900

    faet: porter modal 구현, mypage 제거 및 가이드 페이지 생성

---
+++ src/components/GuideLines.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GuideLines = () => {
+  return (
+    <div>
+      <div className='guide-line'>GuideLines</div>
+      <div className='description'>준비중...</div>
+    </div>
+  );
+};
+
+export default GuideLines;
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,28 +1,7 @@
-import React, { useState } from 'react';
-import Modal from '../components/Modal';
+import React from 'react';
 
 const MyPage = () => {
-  const [modalOn, setModalOn] = useState(false);
-  const openModal = () => {
-    setModalOn(true);
-  };
-  const closeModal = () => {
-    setModalOn(false);
-  };
-
-  return (
-    <div>
-      MyPage
-      <button onClick={openModal}>Open Modal</button>
-      {modalOn && (
-        <Modal
-          visible={modalOn}
-          closable={true}
-          maskClosable={true}
-          onClose={closeModal}></Modal>
-      )}
-    </div>
-  );
+  return <div>MyPage</div>;
 };
 
 export default MyPage;
