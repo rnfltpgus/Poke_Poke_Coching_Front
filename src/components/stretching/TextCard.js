613e4dc8fcd1fbbfd2517c756ca6aa3183a5f4c7
rnfltpgus <jpp901220@gmail.com>
2022-07-14 13:52:31 +0900

    작업 전 현재 상태 커밋

--- src/components/stretching/TextCard.js
+++ src/components/stretching/TextCard.js
@@ -5,9 +5,9 @@ import styled from 'styled-components';
 const TextCard = () => {
   return (
     <CardWrap>
-      <div className='text-area-title'>
+      {/* <div className='text-area-title'>
         위의 스트레칭 자세를 참고하여 스트레칭을 진행해 주세요.
-      </div>
+      </div> */}
       <div className='text-area-description'>
         두 팔을 들어 120'c 각도를 유지한 채로 날개뼈를 모으고 고개를 들면서 팔을
         안쪽으로 당기는 느낌을 받으며 당겨줍니다. 그 자세로 최소 10초 이상
@@ -21,7 +21,7 @@ export default TextCard;
 
 const CardWrap = styled.div`
   margin: auto 30px;
-  padding: 0 10px;
+  padding: 20px 10px;
   border-radius: 10px;
 
   .text-area-title {
